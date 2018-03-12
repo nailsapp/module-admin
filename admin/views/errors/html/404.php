b3e9021d428e51206fe9486aa3cb64756a38d8c6
Pablo de la Peña <p@shedcollective.org>
2018-03-12 10:56:08 +0000

    Added specific 404 view

---
+++ admin/views/errors/html/404.php
@@ -0,0 +1,19 @@
+<?php
+$oAsset = \Nails\Factory::service('Asset');
+$oAsset->clear();
+$oAsset->load('nails.min.css', 'nailsapp/common');
+$oView = \Nails\Factory::service('View');
+$oView->load('structure/header/blank');
+?>
+    <div class="nailsapp-auth login u-center-screen">
+        <div class="panel">
+            <h1 class="panel__header text-center">
+                404 Page Not Found
+            </h1>
+            <div class="panel__body text-center">
+                The page you requested was not found, or your request was invalid.
+            </div>
+        </div>
+    </div>
+<?php
+$oView->load('structure/footer/blank');
