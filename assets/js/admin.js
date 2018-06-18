5883da5491e48d50a9941f12cf04b1a3f9f9130d
Pablo de la Peña <p@shedcollective.org>
2018-06-18 11:38:22 +0100

    Added JS to tidy up index action buttons when there is > 3

--- assets/js/admin.js
+++ assets/js/admin.js
@@ -3,8 +3,10 @@ let _ADMIN;
 
 import DynamicTable from './components/DynamicTable.js';
 import Sortable from './components/Sortable.js';
+import IndexButtons from './components/IndexButtons.js';
 
 _ADMIN = (function() {
     const dynamicTable = new DynamicTable();
     const sortable = new Sortable();
+    const indexButtons = new IndexButtons();
 })();
---
+++ assets/js/components/IndexButtons.js
@@ -0,0 +1,22 @@
+/* export IndexButtons */
+
+/* globals $, jQuery */
+class IndexButtons {
+
+    /**
+     * Construct IndexButtons
+     * @return {IndexButtons}
+     */
+    constructor() {
+        $('td.actions')
+            .each((index, element) => {
+                let $buttons = $('> a', element);
+                if ($buttons.length > 3) {
+                    $buttons.addClass('btn-block');
+                }
+            });
+        return this;
+    }
+}
+
+export default IndexButtons;
