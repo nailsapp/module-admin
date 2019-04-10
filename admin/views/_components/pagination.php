5181ecdc84d6438d02337a015cd0e100612176fd
Pablo de la Peña <p@shedcollective.org>
2019-04-10 10:38:02 +0100

    Updated pagination
    - Added “Record X to Y of Z” stirng
    - Using Pagination factory

--- admin/views/_components/pagination.php
+++ admin/views/_components/pagination.php
@@ -1,76 +1,77 @@
 <div class="pagination clearfix">
-    <?php
+    <small>
+        <?php
 
-    //  Load the library, muy importante!
-    $this->load->library('pagination');
+        use Nails\Factory;
+
+        $iCountPage = $page ?: 1;
+        $iStart     = $iCountPage * $perPage - $perPage;
+        $iEnd       = $iStart + $perPage;
+        $iStart++;
+        if ($iEnd > $totalRows) {
+            $iEnd = $totalRows;
+        }
+        echo 'Records ' . $iStart . ' to ' . $iEnd . ' of ' . number_format($totalRows);
+
+        ?>
+    </small>
+    <?php
 
     //  Configure the Pagination Library
     //  ================================
 
-    $config = [];
-
-    //  The base URL is the current URI plus any existing GET params
-    $config['base_url'] = site_url(uri_string()) . '?';
-
     //  Build the parameters array, use any existing GET params as the base
     $oInput = \Nails\Factory::service('Input');
-    parse_str($oInput->server('QUERY_STRING'), $params);
+    parse_str($oInput->server('QUERY_STRING'), $aParams);
+    $aParams = array_filter($aParams);
+    unset($aParams['page']);
 
-    //  Filter out the useless ones and append to the base URL
-    $params = array_filter($params);
-    unset($params['page']);
-    $config['base_url'] .= http_build_query($params);
+    echo Factory::factory('Pagination')
+        ->initialize([
+            //  The base URL is the current URI plus any existing GET params
+            'base_url'             => site_url(uri_string()) . '?' . http_build_query($aParams),
 
-    //  Other customisations
-    $config['total_rows']           = $totalRows;
-    $config['per_page']             = $perPage;
-    $config['page_query_string']    = true;
-    $config['query_string_segment'] = 'page';
-    $config['num_links']            = 5;
-    $config['use_page_numbers']     = true;
+            //  Other customisations
+            'total_rows'           => $totalRows,
+            'per_page'             => $perPage,
+            'page_query_string'    => true,
+            'query_string_segment' => 'page',
+            'num_links'            => 5,
+            'use_page_numbers'     => true,
 
-    // --------------------------------------------------------------------------
+            //  Surrounding HTML
+            'full_tag_open'        => '<ul>',
+            'full_tag_close'       => '</ul>',
 
-    //  Styling and markup
-    //  ==================
+            //  "First" link
+            'first_link'           => lang('action_first'),
+            'first_tag_open'       => '<li class="page first">',
+            'first_tag_close'      => '</li>',
 
-    //  Surrounding HTML
-    $config['full_tag_open']  = '<ul>';
-    $config['full_tag_close'] = '</ul>';
+            //  "Previous" link
+            'prev_link'            => '&lsaquo;',
+            'prev_tag_open'        => '<li class="page previous">',
+            'prev_tag_close'       => '</li>',
 
-    //  "First" link
-    $config['first_link']      = lang('action_first');
-    $config['first_tag_open']  = '<li class="page first">';
-    $config['first_tag_close'] = '</li>';
+            //  "Next" link
+            'next_link'            => '&rsaquo;',
+            'next_tag_open'        => '<li class="page next">',
+            'next_tag_close'       => '</li>',
 
-    //  "Previous" link
-    $config['prev_link']      = '&lsaquo;';
-    $config['prev_tag_open']  = '<li class="page previous">';
-    $config['prev_tag_close'] = '</li>';
+            //  "Last" link
+            'last_link'            => lang('action_last'),
+            'last_tag_open'        => '<li class="page last">',
+            'last_tag_close'       => '</li>',
 
-    //  "Next" link
-    $config['next_link']      = '&rsaquo;';
-    $config['next_tag_open']  = '<li class="page next">';
-    $config['next_tag_close'] = '</li>';
+            //  Number link markup
+            'num_tag_open'         => '<li class="page">',
+            'num_tag_close'        => '</li>',
 
-    //  "Last" link
-    $config['last_link']      = lang('action_last');
-    $config['last_tag_open']  = '<li class="page last">';
-    $config['last_tag_close'] = '</li>';
-
-    //  Number link markup
-    $config['num_tag_open']  = '<li class="page">';
-    $config['num_tag_close'] = '</li>';
-
-    //  Current page markup
-    $config['cur_tag_open']  = '<li class="page current"><span class="current">';
-    $config['cur_tag_close'] = '</span></li>';
-
-    $this->pagination->initialize($config);
-
-    // --------------------------------------------------------------------------
-
-    echo $this->pagination->create_links();
+            //  Current page markup
+            'cur_tag_open'         => '<li class="page current"><span class="current">',
+            'cur_tag_close'        => '</span></li>',
+        ])
+        ->generate();
 
     ?>
     <div style="clear:both"></div>
