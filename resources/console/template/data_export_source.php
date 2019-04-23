b8eb46b0f46f02e2c8d7c4c4e8b8f6b1acddb007
Pablo de la Peña <p@shedcollective.org>
2019-04-23 13:39:04 +0100

    Adds Console command for creating DataExport Sources

---
+++ resources/console/template/data_export_source.php
@@ -0,0 +1,100 @@
+<?php
+
+/**
+ * This file is the template for the contents of DataExport Sources
+ * Used by the console command when creating DataExport Sources
+ */
+
+return <<<'EOD'
+<?php
+
+namespace App\DataExport\Source;
+
+use Nails\Admin\DataExport\SourceResponse;
+use Nails\Admin\Interfaces\DataExport\Source;
+use Nails\Common\Exception\NailsException;
+use Nails\Factory;
+
+class {{CLASS_NAME}} implements Source
+{
+    /**
+     * Returns the source's label
+     *
+     * @return string
+     */
+    public function getLabel(): string
+    {
+        return '{{NAME}}';
+    }
+
+    // --------------------------------------------------------------------------
+
+    /**
+     * Returns the source's filename
+     *
+     * @return string
+     */
+    public function getFileName(): string
+    {
+        return '{{FILENAME}}';
+    }
+
+    // --------------------------------------------------------------------------
+
+    /**
+     * Returns the source's description
+     *
+     * @return string
+     */
+    public function getDescription(): string
+    {
+        return '{{DESCRIPTION}}';
+    }
+
+    // --------------------------------------------------------------------------
+
+    /**
+     * Returns an array of additional options for the export
+     *
+     * @return array
+     */
+    public function getOptions(): array
+    {
+        return [];
+    }
+
+    // --------------------------------------------------------------------------
+
+    /**
+     * Provides an opportunity for the source to decide whether it is available or not to the user
+     *
+     * @return bool
+     */
+    public function isEnabled(): bool
+    {
+        return true;
+    }
+
+    // --------------------------------------------------------------------------
+
+    /**
+     * Performs the export; must return a SourceResponse object,
+     * or an array of these objects.
+     *
+     * @param array $aOptions The options, in key/value form
+     *
+     * @return SourceResponse|array
+     */
+    public function execute($aOptions = [])
+    {
+        $oResponse = Factory::factory('DataExportSourceResponse', 'nails/module-admin');
+
+        // $oResponse->setFields()
+        // $oResponse->setData($aData)
+        // $oResponse->setSource($oSource);
+
+        return $oResponse;
+    }
+}
+
+EOD;
