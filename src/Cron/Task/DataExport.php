0fd893dcc7eb00d00f4f6357f95389c236d65ca0
Pablo de la Peña <p@shedcollective.org>
2019-05-21 19:06:22 +0100

    Added cron task for data export

---
+++ src/Cron/Task/DataExport.php
@@ -0,0 +1,41 @@
+<?php
+
+/**
+ * The DataExport Cron task
+ *
+ * @package  Nails\Admin
+ * @category Task
+ */
+
+namespace Nails\Admin\Cron\Task;
+
+use Nails\Cron\Task\Base;
+
+/**
+ * Class DataExport
+ *
+ * @package Nails\Admin\Cron\Task
+ */
+class DataExport extends Base
+{
+    /**
+     * The task description
+     *
+     * @var string
+     */
+    const DESCRIPTION = 'Processes any pending data export requests';
+
+    /**
+     * The cron expression of when to run
+     *
+     * @var string
+     */
+    const CRON_EXPRESSION = '* * * * *';
+
+    /**
+     * The console command to execute
+     *
+     * @var string
+     */
+    const CONSOLE_COMMAND = 'admin:dataexport';
+}
