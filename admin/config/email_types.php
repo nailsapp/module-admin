38b7a269b5cf962ae1e599afb5a4c4b54e6582d9
Pablo de la Peña <p@shedcollective.org>
2019-07-18 11:01:41 +0100

    Removes test email configs (now in module-email)

--- admin/config/email_types.php
+++ admin/config/email_types.php
@@ -12,17 +12,6 @@
 
 $config['email_types'] = [
     (object) [
-        'slug'             => 'test_email',
-        'name'             => 'Admin: Test Email',
-        'isUnsubscribable' => false,
-        'description'      => 'Test email template, normally used in admin to test if recipients can receive email sent by the system',
-        'template_header'  => '',
-        'template_body'    => 'admin/Email/templates/test_email',
-        'template_footer'  => '',
-        'default_subject'  => 'Test email sent at {{sentAt}}',
-    ],
-
-    (object) [
         'slug'             => 'data_export',
         'name'             => 'Admin: Data Export',
         'isUnsubscribable' => false,
