beeec465f6a714ffa88ac6502fe692a804257a24
Pablo de la Peña <p@shedcollective.org>
2020-09-21 14:58:40 +0100

    Defining test data for data export email

--- src/Factory/Email/DataExport.php
+++ src/Factory/Email/DataExport.php
@@ -7,4 +7,18 @@ use Nails\Email\Factory\Email;
 class DataExport extends Email
 {
     protected $sType = 'data_export';
+
+    // --------------------------------------------------------------------------
+
+    /**
+     * Returns test data to use when sending test emails
+     *
+     * @return array
+     */
+    public function getTestData(): array
+    {
+        return [
+            'status' => 'COMPLETE',
+        ];
+    }
 }
