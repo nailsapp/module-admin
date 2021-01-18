f18684c42e33e58ea29ce0788f67afb0762def4d
Pablo de la Peña <p@shedcollective.org>
2021-01-18 12:15:43 +0000

    Adds IndexFilters for changeLog (users and types)

--- admin/controllers/ChangeLog.php
+++ admin/controllers/ChangeLog.php
@@ -3,6 +3,12 @@
 namespace Nails\Admin\Admin;
 
 use Nails\Admin\Controller\DefaultController;
+use Nails\Admin\Factory\IndexFilter;
+use Nails\Auth;
+use Nails\Common\Exception\FactoryException;
+use Nails\Common\Exception\ModelException;
+use Nails\Common\Service\Database;
+use Nails\Factory;
 
 /**
  * Class ChangeLog
@@ -18,6 +24,7 @@ class ChangeLog extends DefaultController
     const CONFIG_SORT_OPTIONS   = [
         'Created' => 'created',
     ];
+    const CONFIG_SORT_DIRECTION = self::SORT_DESCENDING;
     const CONFIG_INDEX_FIELDS   = [
         'User'    => 'user',
         'Changes' => null,
@@ -32,6 +39,7 @@ class ChangeLog extends DefaultController
     const CONFIG_CAN_DELETE     = false;
     const CONFIG_CAN_EDIT       = false;
     const CONFIG_CAN_VIEW       = false;
+    const CONFIG_PERMISSION     = 'admin:logs:change';
 
     // --------------------------------------------------------------------------
 
@@ -97,5 +105,122 @@ class ChangeLog extends DefaultController
                 );
             }, json_decode($oLog->changes) ?? []))
         );
+    }
+
+    // --------------------------------------------------------------------------
+
+    /**
+     * Adds filters to the idnex page
+     *
+     * @return array
+     * @throws FactoryException
+     * @throws ModelException
+     */
+    protected function indexDropdownFilters(): array
+    {
+        return array_merge(
+            parent::indexDropdownFilters(),
+            array_filter([
+                $this->filterByUser(),
+                $this->filterByEntity(),
+            ])
+        );
+    }
+
+    // --------------------------------------------------------------------------
+
+    /**
+     * Returns an IndexFilter for the users who have made changes
+     *
+     * @return IndexFilter|null
+     * @throws FactoryException
+     * @throws ModelException
+     */
+    protected function filterByUser(): ?IndexFilter
+    {
+        /** @var Database $oDb */
+        $oDb = Factory::service('Database');
+        /** @var \Nails\Admin\Model\ChangeLog $oChangeLogModel */
+        $oChangeLogModel = static::getModel();
+        /** @var Auth\Model\User $oUserModel */
+        $oUserModel = Factory::model('User', Auth\Constants::MODULE_SLUG);
+
+        $aUserIds = $oDb
+            ->select('DISTINCT(user_id)')
+            ->get($oChangeLogModel->getTableName())->result();
+
+        $aUsers = $oUserModel->getByIds(arrayExtractProperty($aUserIds, 'user_id'));
+
+        if (empty($aUsers) || count($aUsers) === 1) {
+            return null;
+        }
+
+        /** @var IndexFilter $oFilter */
+        $oFilter = Factory::factory('IndexFilter', 'nails/module-admin');
+        $oFilter
+            ->setLabel('User')
+            ->setColumn('user_id')
+            ->addOption('Everyone')
+            ->addOptions(array_map(function (Auth\Resource\User $oUser) {
+                /** @var IndexFilter\Option $oOption */
+                $oOption = Factory::factory('IndexFilterOption', 'nails/module-admin');
+                $oOption
+                    ->setLabel(sprintf(
+                        '#%s - %s (%s)',
+                        $oUser->id,
+                        $oUser->name,
+                        $oUser->email
+                    ))
+                    ->setValue($oUser->id);
+
+                return $oOption;
+            }, $aUsers));
+
+        return $oFilter;
+    }
+
+    // --------------------------------------------------------------------------
+
+    /**
+     * Returns an IndexFilter for the various types of entities in the changelog
+     *
+     * @return IndexFilter|null
+     * @throws FactoryException
+     * @throws ModelException
+     */
+    protected function filterByEntity(): ?IndexFilter
+    {
+        /** @var Database $oDb */
+        $oDb = Factory::service('Database');
+        /** @var \Nails\Admin\Model\ChangeLog $oChangeLogModel */
+        $oChangeLogModel = static::getModel();
+
+        $aEntities = $oDb
+            ->select('DISTINCT(item)')
+            ->get($oChangeLogModel->getTableName())->result();
+
+        $aEntities = arrayExtractProperty($aEntities, 'item');
+
+        if (empty($aEntities) || count($aEntities) === 1) {
+            return null;
+        }
+
+        /** @var IndexFilter $oFilter */
+        $oFilter = Factory::factory('IndexFilter', 'nails/module-admin');
+        $oFilter
+            ->setLabel('Type')
+            ->setColumn('item')
+            ->addOption('All')
+            ->addOptions(array_map(function (string $sType) {
+                /** @var IndexFilter\Option $oOption */
+                $oOption = Factory::factory('IndexFilterOption', 'nails/module-admin');
+                $oOption
+                    ->setLabel($sType)
+                    ->setValue($sType);
+
+                return $oOption;
+            }, $aEntities));
+
+        return $oFilter;
     }
 }
