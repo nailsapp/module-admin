b4c84a4afaccf760a7d30cafb912e7495d4b6b2b
Pablo de la Peña <p@shedcollective.org>
2021-03-22 21:58:01 +0000

    Enforcing class hierarchy when using App overrides

--- admin/controllers/AdminRouter.php
+++ admin/controllers/AdminRouter.php
@@ -25,6 +25,17 @@ use Nails\Factory;
 if (class_exists('\App\Admin\Controller\BaseRouter')) {
     abstract class BaseMiddle extends \App\Admin\Controller\BaseRouter
     {
+        public function __construct()
+        {
+            if (!classExtends(parent::class, \Nails\Common\Controller\Base::class)) {
+                throw new NailsException(sprintf(
+                    'Class %s must extend %s',
+                    parent::class,
+                    \Nails\Common\Controller\Base::class
+                ));
+            }
+            parent::__construct();
+        }
     }
 } else {
     abstract class BaseMiddle extends \Nails\Common\Controller\Base
