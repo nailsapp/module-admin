7b95bd2a89bb8d2eb7ca8942db20dff2b8a1d139
Pablo de la Peña <p@shedcollective.org>
2021-03-26 17:08:35 +0000

    Remove admin.expor JS

--- admin/controllers/Utilities.php
+++ admin/controllers/Utilities.php
@@ -246,10 +246,6 @@ class Utilities extends Base
 
         // --------------------------------------------------------------------------
 
-        //  Load assets
-        $oAsset = Factory::service('Asset');
-        $oAsset->load('nails.admin.export.min.js', \Nails\Asset\Constants::MODULE_SLUG);
-
         //  Load views
         Helper::loadView('export/index');
     }
